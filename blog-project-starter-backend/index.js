3a29555e0addb91a48ff9a290f13d3bbf3efd771
dhivyx <dhivya16112002gmail.com>
2025-12-05 17:46:20 +0530

    changes made

--- blog-project-starter-backend/index.js
+++ blog-project-starter-backend/index.js
@@ -4,13 +4,13 @@ const mongoose = require('mongoose');
 const cors = require("cors")
 
 const app = express();
-app.use(cors())
+app.use(cors("https://blog-portfolio-o6lk.vercel.app/"))
 
 // Middleware
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect('mongodb://localhost:27017/blogDB').then(() => {
+mongoose.connect(process.env.MONGO_URL).then(() => {
   console.log("Connection Successfull")
 })
 
@@ -75,4 +75,9 @@ app.post('/api/blogs', async (req, res) => {
 });
 
 // Start server
-app.listen(5000, () => console.log('Server running on port 5000'));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
