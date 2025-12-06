99fbcd527cad26697e4776dcf573b19f71650aac
dhivyx <132746536+dhivyx@users.noreply.github.com>
2025-12-06 21:37:46 +0530

    Changes made

--- blog-project-starter-backend/index.js
+++ blog-project-starter-backend/index.js
@@ -8,7 +8,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-app.use(cors("https://blog-portfolio-o6lk.vercel.app/"))
+app.use(cors("https://blog-portfolio-o6lk.vercel.app"))
 
 // Middleware
 app.use(express.json());
